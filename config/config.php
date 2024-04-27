c47b974f8917eb5ad87b177b528674b8a141e6a5
altamisatmaja <altamisfattah16@gmail.com>
2024-04-27 17:48:32 +0700

    fix: env

--- config/config.php
+++ config/config.php
@@ -1,17 +1,18 @@
 <?php
 
-$file = '../config/config.properties';
-if (!file_exists($file)) {
-    echo 'File tidak ditemukan';
-    exit();
-}
+// $file = '../config/config.properties';
+// if (!file_exists($file)) {
+//     echo 'File tidak ditemukan';
+//     exit();
+// }
 
-$properties = parse_ini_file($file);
+// $properties = parse_ini_file($file);
+(new Dotenv(__DIR__.'/../.env'))->load();
 
-define('BASE_URL', $properties['url']);
+define('BASE_URL', getenv('BASE_URL'));
 
-define('DB_HOST', $properties['dbhost']);
-define('DB_USER', $properties['dbuser']);
-define('DB_PASSWORD', $properties['dbpass']);
-define('DB_NAME', $properties['dbname']);
-define('DB_PORT', $properties['dbport']);
+// define('DB_HOST', $properties['dbhost']);
+// define('DB_USER', $properties['dbuser']);
+// define('DB_PASSWORD', $properties['dbpass']);
+// define('DB_NAME', $properties['dbname']);
+// define('DB_PORT', $properties['dbport']);
