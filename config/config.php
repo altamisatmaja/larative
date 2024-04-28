a4cc7e9349383b9cbe22b33a26504b0a1f32d607
altamisatmaja <altamisfattah16@gmail.com>
2024-04-29 00:05:40 +0700

    fix: query builder

--- config/config.php
+++ config/config.php
@@ -5,20 +5,22 @@
 //     echo 'File tidak ditemukan';
 //     exit();
 // }
+// $file = '../config/config.properties';
+// if (!file_exists($file)) {
+//     echo 'File tidak ditemukan';
+//     exit();
+// }
 
 // $properties = parse_ini_file($file);
-<<<<<<< HEAD
 (new Dotenv(__DIR__.'/../.env'))->load();
 
 define('BASE_URL', getenv('BASE_URL'));
 
-=======
-
-(new Dotenv(__DIR__.'../.env'))->load();
-
-define('BASE_URL', getenv('BASE_URL'));
-
->>>>>>> 6d8386e5c15f6c42000d3b43bfc084a0af8ffcca
+// define('DB_HOST', $properties['dbhost']);
+// define('DB_USER', $properties['dbuser']);
+// define('DB_PASSWORD', $properties['dbpass']);
+// define('DB_NAME', $properties['dbname']);
+// define('DB_PORT', $properties['dbport']);
 // define('DB_HOST', $properties['dbhost']);
 // define('DB_USER', $properties['dbuser']);
 // define('DB_PASSWORD', $properties['dbpass']);
